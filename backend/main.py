d1d23a3572bf0a9c381c4f89bcb7a7d7e43f14d4
Jaival <jaivalchauhan111@gmail.com>
2025-07-07 22:54:52 +0530

    fixing deploy errors

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from database import engine
 import models
 from routers import user_auth, spotify_auth
@@ -12,8 +11,6 @@ app.include_router(spotify_auth.router)
 
 origins = [
     "https://subsense.vercel.app",
-    "https://subsense.vercel.app/login",
-    "https://subsense.vercel.app/signup"
 ]
 
 app.add_middleware(
@@ -23,11 +20,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"],
-)
-
-app.add_middleware(
-    TrustedHostMiddleware,
-    allowed_hosts=["*"]
 )
 
 models.Base.metadata.create_all(bind=engine)
